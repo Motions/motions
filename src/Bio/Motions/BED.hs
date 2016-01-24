581a3753374563b78b67c7294e18dcff2edd1a8e
Rafał Stefański <ravst94@gmail.com>
2016-01-25 00:52:04 +0100

    BED to EnegyVector parsing
    Change-Id: I654c3a7c8086c46f4aa6ed7a2391d274607f5bc9

---
+++ src/Bio/Motions/BED.hs
@@ -0,0 +1,103 @@
+{- |
+Module      : Bio.Motions.BED
+Description : Parsing BED into chain of beads
+License     : MIT
+Stability   : experimental
+Portability : unportable
+ -}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TupleSections #-}
+module Bio.Motions.BED where
+
+import Bio.Motions.Types
+
+import qualified Control.Applicative as A
+import Control.Monad.State
+import Control.Monad.Random
+import Control.Monad.Trans.Maybe
+import Control.Monad.Except
+import Data.Maybe
+import Text.ParserCombinators.Parsec
+import Foreign.Marshal.Utils
+import qualified Data.Map as M
+import qualified Data.Vector.Unboxed as U
+
+import Text.Read
+
+-- |Represents a binding site
+data BindingSiteInfo = BindingSiteInfo
+    { bsChain :: Int
+    , bsFrom :: Int
+    , bsTo :: Int
+    , bsType :: Int
+    }
+    deriving (Eq, Show)
+
+-- |Combines BED files into EnergyVectors of beads
+parseBEDs ::
+     Int
+  -- ^ Resolution of simulation
+  -> [Int]
+  -- ^ Lenghts of chromosomes (bp)
+  -> [FilePath]
+  -- ^ Locations of BED files
+  -> IO [[EnergyVector]]
+parseBEDs resolution lengths fileNames = do
+  parses <- zipWithM (parseFromFile . parseBED) [0..] fileNames
+  beds <- concat <$> mapM (either (ioError . userError . show) return) parses
+  let newLengths = map (`divCeil` resolution) lengths
+  let bsInfos = map (applyResolution resolution) beds
+  return $ collect (length fileNames) newLengths bsInfos
+
+-- |Parses a single BED file
+parseBED :: Int -> Parser [BindingSiteInfo]
+parseBED bsType = do
+  optional $ string "Track" >> manyTill anyChar eol
+  endBy (line bsType) eol
+
+-- |Reads end of line
+eol :: Parser String
+eol = try (string "\n\r")
+     <|> try (string "\r\n")
+     <|> string "\r"
+     <|> string "\n"
+     <?> "End of line"
+
+-- |Parses one line of BED
+line :: Int -> Parser BindingSiteInfo
+line bsType = do
+  bsChain <- chromosome
+  tab
+  bsFrom <- parseInt
+  tab
+  bsTo <- parseInt
+  unless (bsFrom <= bsTo) $ fail "Binding site ends before it begins"
+  optional (tab >> manyTill anyChar (lookAhead eol))
+  return BindingSiteInfo{..}
+
+parseInt :: Parser Int
+parseInt = (read A.<$> many1 digit) <?> "non-negative integer"
+
+-- |Parses BED 'chromosome' column
+chromosome :: Parser Int
+chromosome = optional (string "chr") >> (\x -> x-1) A.<$> parseInt
+
+-- |Groups nucleotides according to the resolution parameter
+applyResolution :: Int -> BindingSiteInfo -> BindingSiteInfo
+applyResolution resolution x@BindingSiteInfo{..} =
+  x {bsFrom = bsFrom `div` resolution, bsTo = bsTo `div` resolution}
+
+-- |Ceil of a quotient
+divCeil :: (Integral a) => a -> a -> a
+divCeil x y = fromBool (x `mod` y > 0) + div x y
+
+-- |Gathers information about all binding sites and returns a EnergyVector for each bead
+-- in the simulation
+collect :: Int -> [Int] -> [BindingSiteInfo] -> [[EnergyVector]]
+collect typesCount lengths bsInfos =
+  [[EnergyVector . U.fromList $ [M.findWithDefault 0 (chr, pos, bsType) dict
+      | bsType <- [0..typesCount - 1]] | pos <- [0..chrLen - 1]] | (chr, chrLen) <- zip [0..] lengths]
+  where
+    expandBsInfos = concat [map (bsChain,,bsType) [bsFrom..bsTo] | BindingSiteInfo{..} <- bsInfos]
+    dict = foldl incrementMap M.empty expandBsInfos
+    incrementMap m new = M.alter (Just . maybe 1 (+1)) new m
---
+++ test/BEDSpec.hs
@@ -0,0 +1,36 @@
+module BEDSpec where
+
+import Test.Hspec
+import Bio.Motions.BED
+import Bio.Motions.Types
+import Text.ParserCombinators.Parsec
+import qualified Data.Vector.Unboxed as U
+
+test :: Spec
+test = do
+  context "when parsing BED file" $ do
+    ansOK <- runIO $ parseFromFile (parseBED 1) "test/testdata/1.bed"
+    it "should parse file without errors" $
+      ansOK `shouldBe` Right [
+        BindingSiteInfo {bsChain = 0, bsFrom = 1, bsTo = 9, bsType = 1},
+        BindingSiteInfo {bsChain = 1, bsFrom = 10, bsTo = 20, bsType = 1},
+        BindingSiteInfo {bsChain = 2, bsFrom = 5, bsTo = 18, bsType = 1}]
+    ansErr <- runIO $ parseFromFile (parseBED 1) "test/testdata/wrong.bed"
+    it "should not parse file with errors" $
+      ansErr `shouldNotSatisfy` eitherToBool
+  context "when creating a lists of EnergyVectors" $ do
+    ans <- runIO $ parseBEDs 10 [50, 40, 20] ["test/testdata/0.bed", "test/testdata/1.bed"]
+    it "should calculate energy vectors correctly" $
+      ans `shouldBe` map (map $ EnergyVector . U.fromList) [
+        [[0,1],[1,0],[2,0],[1,0],[1,0]],
+        [[0,0],[1,1],[1,1],[0,0]],
+        [[0,1],[1,1]]]
+    let ansErr = parseBEDs 10 [50, 40, 20] ["test/testdata/wrong.bed"]
+    it "should throw an exception when getting an invalid file" $
+      ansErr `shouldThrow` anyIOException
+
+eitherToBool :: Either a b -> Bool
+eitherToBool = either (const False) (const True)
+
+spec :: Spec
+spec = describe "BED parser" test
