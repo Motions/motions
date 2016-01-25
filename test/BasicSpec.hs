01cb47ae7c23a16a833a1916aee07e5a1995f2af
Krzysztof Pszeniczny <krzysztof.pszeniczny@gmail.com>
2016-01-25 20:30:13 +0100

    Mutable representation
    Change-Id: I70916e41c7ddc9b20d017b0fc76cdc1b51810183

--- test/BasicSpec.hs
+++ test/BasicSpec.hs
@@ -15,6 +15,7 @@ import Test.Hspec
 import Bio.Motions.Types
 import Bio.Motions.Representation.Class
 import Bio.Motions.Representation.Chain.Internal (PureChainRepresentation, intersectsChain)
+import Bio.Motions.Representation.Chain.Mutable
 import Bio.Motions.Callback.Class
 import Bio.Motions.Callback.StandardScore
 import Bio.Motions.Callback.Parser.TH
@@ -23,6 +24,7 @@ import qualified Bio.Motions.Representation.Dump as D
 import Control.Monad
 import Data.MonoTraversable
 import Data.Proxy
+import qualified Data.Map.Strict as M
 import qualified Data.Vector.Unboxed as U
 import Linear
 
@@ -66,136 +68,46 @@ import GHC.TypeLits
     COMPUTE LIST DIST(X 0, X 1)
 |]
 
-testRepr :: Spec
-testRepr = do
-    repr :: PureChainRepresentation <- loadDump dump
+testIntersectsChain :: Spec
+testIntersectsChain = do
+    it "reports actual intersections to exist" $
+        intersectsChain space (V3 7 8 7) (V3 7 7 8) `shouldBe` True
+    it "doesn't report about non-existing intersections" $
+        intersectsChain space (V3 7 8 7) (V3 7 8 8) `shouldBe` False
+  where
+    space = M.fromList
+        [ (V3 7 7 7, Bead $ BeadInfo (V3 7 7 7) be ev 0 0 0)
+        , (V3 7 8 8, Bead $ BeadInfo (V3 7 8 8) be ev 0 0 1)
+        ]
+    be = BeadType 0
+    ev = EnergyVector U.empty
 
-    context "when redumping" $ do
-        dump' <- makeDump repr
-        it "yields the same radius" $
-            D.radius dump' `shouldBe` D.radius dump
-        it "yields the same bead kinds" $
-            D.beadKinds dump' `shouldBe` D.beadKinds dump
-        it "yields the same chains" $
-            D.chains dump' `shouldBe` D.chains dump
-        it "yields the same binders" $
-            D.binders dump' `shouldMatchList` D.binders dump
+testRepr :: forall proxy repr. Representation IO repr => proxy repr -> Spec
+testRepr _ = before (loadDump dump :: IO repr) $ do
+    context "when redumping" $
+        beforeWith makeDump testRedump
 
-    context "when inspecting the data" $ do
-        it "yields the same number of chains" $
-            getNumberOfChains repr >>= flip shouldBe (length $ D.chains dump)
-        it "yields the same binders" $ do
-            binders <- getBinders repr (pure . otoList)
-            binders `shouldBe` D.binders dump
-        it "yields the same beads" $ do
-            beads <- forM [0..length (D.chains dump) - 1] $
-                \idx -> getChain repr idx (pure . otoList)
-            beads `shouldBe` D.chains dump
+    context "when inspecting the data"
+        testInspect
 
-        context "when using getAtomAt" $ do
-            it "returns binders" $
-                forM_ (D.binders dump) $ \binder -> do
-                    atom <- getAtomAt (binderPosition binder) repr
-                    atom `shouldBe` Just (Binder binder)
-            it "returns beads" $
-                forM_ (concat $ D.chains dump) $ \bead -> do
-                    atom <- getAtomAt (beadPosition bead) repr
-                    atom `shouldBe` Just (Bead bead)
-            it "returns Nothing" $ do
-                atom <- getAtomAt (V3 0 0 0) repr
-                atom `shouldBe` Nothing
+    context "when computing callbacks"
+        testCallbacks
 
-    context "when computing callbacks" $ do
-        it "has the correct score" $ do
-            score :: StandardScore <- runCallback repr
-            score `shouldBe` 1002
+    beforeWith (\repr -> fst <$> performMove (Move (V3 5 6 6) (V3 0 0 (-1))) repr) $
+        context "after making a bead move" $ do
+            testAfterBeadMove
 
-        it "has the correct score after a bead move" $ do
-            score :: StandardScore <- updateCallback repr 1002 $ Move (V3 5 6 6) (V3 0 0 (-1))
-            score `shouldBe` 2002
-
-        it "has the correct score after a binder move" $ do
-            score :: StandardScore <- updateCallback repr 1002 $ Move (V3 0 1 2) (V3 1 0 0)
-            score `shouldBe` 1000
-
-        context "when computing the template haskell callbacks" $ do
-            it "has the correct sum42-beads" $ do
-                res :: THCallback "sum42-beads" <- runCallback repr
-                res `shouldBe` THCallback (42 * beads)
-
-            it "has the correct prod2-all" $ do
-                res :: THCallback "prod2-all" <- runCallback repr
-                res `shouldBe` THCallback (2 ^ (beads + binders))
-
-            it "has the correct list42-binders" $ do
-                res :: THCallback "list42-binders" <- runCallback repr
-                res `shouldBe` THCallback (replicate binders 42)
-
-            it "has the correct prod-binders-beads" $ do
-                res :: THCallback "prod-binders-beads" <- runCallback repr
-                res `shouldBe` THCallback (binders * beads)
-
-            it "has the correct list-11" $ do
-                res :: THCallback "list-11" <- runCallback repr
-                res `shouldBe` THCallback [sqrt 2, 1]
-
-
-    repr' <- fst <$> performMove (Move (V3 5 6 6) (V3 0 0 (-1))) repr
-    dump' <- makeDump repr'
-
-    context "when performing a bead move" $ do
-        it "reports the old location to be empty" $ do
-            matom <- getAtomAt (V3 5 6 6) repr'
-            matom `shouldBe` Nothing
-        it "reports the new location to contain the bead" $ do
-            matom <- getAtomAt (V3 5 6 5) repr'
-            matom `shouldBe` Just (Bead $ BeadInfo (V3 5 6 5) be1 ev1 1 0 1)
-        it "reports the updated chain" $ do
-            chain <- getChain repr' 0 $ pure . otoList
-            chain `shouldBe` [ BeadInfo (V3 0 1 1) be0 ev0 0 0 0
-                             , BeadInfo (V3 5 6 5) be1 ev1 1 0 1
-                             , BeadInfo (V3 5 5 6) be0 ev0 2 0 2
-                             ]
-            chain `shouldBe` head (D.chains dump')
-        it "reports the binders to be unchanged" $ do
-            binders <- getBinders repr' $ pure . otoList
-            binders `shouldMatchList` D.binders dump
-            binders `shouldMatchList` D.binders dump'
-
-    repr'' <- fst <$> performMove (Move (V3 0 1 2) (V3 1 0 0)) repr'
-    dump'' <- makeDump repr''
-
-    context "when performing a binder move" $ do
-        it "reports the old location to be empty" $ do
-            matom <- getAtomAt (V3 0 1 2) repr''
-            matom `shouldBe` Nothing
-        it "reports the new location to contain the binder" $ do
-            matom <- getAtomAt (V3 1 1 2) repr''
-            matom `shouldBe` Just (Binder $ BinderInfo (V3 1 1 2) bi0)
-        it "reports the updated binders" $ do
-            binders <- getBinders repr'' $ pure . otoList
-            binders `shouldMatchList` [ BinderInfo (V3 1 1 2) bi0
-                                      , BinderInfo (V3 0 1 3) bi0
-                                      , BinderInfo (V3 5 5 5) bi1
-                                      ]
-            binders `shouldMatchList` D.binders dump''
-        it "reports the beads to be unchanged" $
-            D.chains dump'' `shouldBe` D.chains dump'
-    context "when checking for intersections" $ do
-        it "reports actual intersections to exist" $
-            intersectsChain repr'' (V3 7 8 7) (V3 7 7 8) `shouldBe` True
-        it "doesn't report about non-existing intersections" $
-            intersectsChain repr'' (V3 7 8 7) (V3 7 8 8) `shouldBe` False
-
+            beforeWith (\repr -> fst <$> performMove (Move (V3 0 1 2) (V3 1 0 0)) repr) $
+                context "after making a bead move" testAfterBinderMove
   where
     beads = sum $ map length $ D.chains dump
     binders = length $ D.binders dump
     dump = D.Dump
         { D.radius = 10
         , D.binders =
-            [ BinderInfo (V3 0 1 2) bi0
-            , BinderInfo (V3 0 1 3) bi0
-            , BinderInfo (V3 5 5 5) bi1
+            [ BinderInfo (V3 0 1 2) bi0 0
+            , BinderInfo (V3 0 1 3) bi0 1
+            , BinderInfo (V3 5 5 5) bi1 2
             ]
         , D.chains =
             [ [ BeadInfo (V3 0 1 1) be0 ev0 0 0 0
@@ -215,5 +127,130 @@ testRepr = do
     [be0, be1] = BeadType <$> [0, 1]
     [ev0, ev1] = EnergyVector . U.fromList <$> [[1, 0], [0, 1000]]
 
+    updatedChain = [ BeadInfo (V3 0 1 1) be0 ev0 0 0 0
+                   , BeadInfo (V3 5 6 5) be1 ev1 1 0 1
+                   , BeadInfo (V3 5 5 6) be0 ev0 2 0 2
+                   ]
+
+    updatedChains = updatedChain : tail (D.chains dump)
+
+    updatedBinders = [ BinderInfo (V3 1 1 2) bi0 0
+                     , BinderInfo (V3 0 1 3) bi0 1
+                     , BinderInfo (V3 5 5 5) bi1 2
+                     ]
+
+    testRedump :: SpecWith D.Dump
+    testRedump = do
+        it "yields the same radius" $ \dump' ->
+            D.radius dump' `shouldBe` D.radius dump
+        it "yields the same bead kinds" $ \dump' ->
+            D.beadKinds dump' `shouldBe` D.beadKinds dump
+        it "yields the same chains" $ \dump' ->
+            D.chains dump' `shouldBe` D.chains dump
+        it "yields the same binders" $ \dump' ->
+            D.binders dump' `shouldMatchList` D.binders dump
+
+    testInspect :: SpecWith repr
+    testInspect = do
+        it "yields the same number of chains" $
+            getNumberOfChains >=> (`shouldBe` length (D.chains dump))
+        it "yields the same binders" $ \repr -> do
+            binders <- getBinders repr (pure . otoList)
+            binders `shouldBe` D.binders dump
+        it "yields the same beads" $ \repr -> do
+            beads <- forM [0..length (D.chains dump) - 1] $
+                \idx -> getChain repr idx (pure . otoList)
+            beads `shouldBe` D.chains dump
+
+        context "when using getAtomAt" $ do
+            it "returns binders" $ \repr ->
+                forM_ (D.binders dump) $ \binder -> do
+                    atom <- getAtomAt (binderPosition binder) repr
+                    atom `shouldBe` Just (Binder binder)
+            it "returns beads" $ \repr ->
+                forM_ (concat $ D.chains dump) $ \bead -> do
+                    atom <- getAtomAt (beadPosition bead) repr
+                    atom `shouldBe` Just (Bead bead)
+            it "returns Nothing" $ \repr -> do
+                atom <- getAtomAt (V3 0 0 0) repr
+                atom `shouldBe` Nothing
+
+    testCallbacks :: SpecWith repr
+    testCallbacks = do
+        it "has the correct score" $ \repr -> do
+            score :: StandardScore <- runCallback repr
+            score `shouldBe` 1002
+
+        it "has the correct score after a bead move" $ \repr -> do
+            score :: StandardScore <- updateCallback repr 1002 $ Move (V3 5 6 6) (V3 0 0 (-1))
+            score `shouldBe` 2002
+
+        it "has the correct score after a binder move" $ \repr -> do
+            score :: StandardScore <- updateCallback repr 1002 $ Move (V3 0 1 2) (V3 1 0 0)
+            score `shouldBe` 1000
+
+        context "when computing the template haskell callbacks" $ do
+            it "has the correct sum42-beads" $ \repr -> do
+                res :: THCallback "sum42-beads" <- runCallback repr
+                res `shouldBe` THCallback (42 * beads)
+
+            it "has the correct prod2-all" $ \repr -> do
+                res :: THCallback "prod2-all" <- runCallback repr
+                res `shouldBe` THCallback (2 ^ (beads + binders))
+
+            it "has the correct list42-binders" $ \repr -> do
+                res :: THCallback "list42-binders" <- runCallback repr
+                res `shouldBe` THCallback (replicate binders 42)
+
+            it "has the correct prod-binders-beads" $ \repr -> do
+                res :: THCallback "prod-binders-beads" <- runCallback repr
+                res `shouldBe` THCallback (binders * beads)
+
+            it "has the correct list-11" $ \repr -> do
+                res :: THCallback "list-11" <- runCallback repr
+                res `shouldBe` THCallback [sqrt 2, 1]
+
+    testAfterBeadMove :: SpecWith repr
+    testAfterBeadMove = do
+        it "reports the old location to be empty" $ \repr -> do
+            matom <- getAtomAt (V3 5 6 6) repr
+            matom `shouldBe` Nothing
+        it "reports the new location to contain the bead" $ \repr -> do
+            matom <- getAtomAt (V3 5 6 5) repr
+            matom `shouldBe` Just (Bead $ BeadInfo (V3 5 6 5) be1 ev1 1 0 1)
+        it "reports the updated chain" $ \repr -> do
+            chain <- getChain repr 0 $ pure . otoList
+            chain `shouldBe` updatedChain
+        it "reports the binders to be unchanged" $ \repr -> do
+            binders <- getBinders repr $ pure . otoList
+            binders `shouldMatchList` D.binders dump
+
+        context "when dumping" $ beforeWith makeDump $ do
+            it "reports the updated chain" $ \dump' ->
+                D.chains dump' `shouldBe` updatedChains
+            it "reports the binders to be unchanged" $ \dump' ->
+                D.binders dump' `shouldMatchList` D.binders dump
+
+    testAfterBinderMove :: SpecWith repr
+    testAfterBinderMove = do
+        it "reports the old location to be empty" $ \repr -> do
+            matom <- getAtomAt (V3 0 1 2) repr
+            matom `shouldBe` Nothing
+        it "reports the new location to contain the binder" $ \repr -> do
+            matom <- getAtomAt (V3 1 1 2) repr
+            matom `shouldBe` Just (Binder $ BinderInfo (V3 1 1 2) bi0 0)
+        it "reports the updated binders" $ \repr -> do
+            binders <- getBinders repr $ pure . otoList
+            binders `shouldMatchList` updatedBinders
+
+        context "when dumping" $ beforeWith makeDump $ do
+            it "reports the beads to be unchanged" $ \dump' ->
+                D.chains dump' `shouldBe` updatedChains
+            it "reports the updated binders" $ \dump' ->
+                D.binders dump' `shouldMatchList` updatedBinders
+
 spec :: Spec
-spec = context "the pure chain representation" testRepr
+spec = do
+    context "the pure chain representation" $ testRepr (Proxy :: Proxy PureChainRepresentation)
+    context "the IO chain representation" $ testRepr (Proxy :: Proxy IOChainRepresentation)
+    context "the intersectsChain function" testIntersectsChain
