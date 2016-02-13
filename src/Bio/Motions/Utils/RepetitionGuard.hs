701e8c694f28e605b7dcdcc860990df6a66ff80c
Rafał Stefański <ravst94@gmail.com>
2016-02-13 01:52:55 +0000

    State initialisation
    Change-Id: I1f8f8f1e42ca0b251aef2f0b822ce26f89eedfff

---
+++ src/Bio/Motions/Utils/RepetitionGuard.hs
@@ -0,0 +1,28 @@
+{- |
+Module      : Bio.Motions.Utils.RepetitionGuard
+Description : Monad transformer used to limit the number of repetitions.
+License     : MIT
+Stability   : experimental
+Portability : unportable
+-}
+
+module Bio.Motions.Utils.RepetitionGuard where
+
+import Control.Applicative
+import Control.Monad.State
+import Control.Monad.Trans
+import Control.Monad.Trans.Maybe
+
+-- |Transforms a 'Monad' 'm' into a monad that limits the number of 'nextRep's
+type RepetitionGuardT m = MaybeT (StateT Int m)
+
+-- |Returns 'Nothing' iff the limit for counted operations was exceeded
+runRepetitionGuardT :: (Monad m) => Int -> RepetitionGuardT m a -> m (Maybe a)
+runRepetitionGuardT opsLimit = (`evalStateT` opsLimit) . runMaybeT
+
+-- |Marks a counted operation
+nextRep :: (Monad m) => RepetitionGuardT m ()
+nextRep = do
+  count <- get
+  guard $ count >= 0
+  put $ count - 1
