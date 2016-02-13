7f54d00d297f28457639d78bf5c6a685775d648f
Rafał Stefański <ravst94@gmail.com>
2016-02-13 01:52:55 +0000

    State initialisation
    Change-Id: I1f8f8f1e42ca0b251aef2f0b822ce26f89eedfff

---
+++ src/Bio/Motions/StateInitialisation.hs
@@ -0,0 +1,175 @@
+{- |
+Module      : Bio.Motions.StateInitialisation
+Description : Random initialisation of state for new simulation.
+License     : MIT
+Stability   : experimental
+Portability : unportable
+-}
+
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE RecordWildCards #-}
+module Bio.Motions.StateInitialisation where
+
+import Control.Lens
+import Control.Monad.Random
+import Control.Monad.State.Lazy
+import Control.Monad.Trans.Maybe
+import Data.List
+import qualified Data.Map as M
+import Data.Maybe
+import qualified Data.Vector.Unboxed as U
+import qualified Data.Vector as V
+import GHC.Exts(groupWith, sortWith)
+import Linear
+import System.Random.Shuffle
+
+import Bio.Motions.Common
+import Bio.Motions.Types
+import Bio.Motions.Representation.Common
+import Bio.Motions.Representation.Dump
+import Bio.Motions.Utils.RepetitionGuard
+
+-- |Creates an initial state
+initialise :: (MonadRandom m) =>
+     Int
+  -- ^Maximum number of initialization attempts
+  -> Int
+  -- ^Radius
+  -> Int
+  -- ^Number of binders
+  -> Int
+  -- ^Number of binders' types
+  -> [[EnergyVector]]
+  -- ^EnergyVectors of beads
+  -> m (Maybe Dump)
+  -- ^Initial state
+initialise maxTries r bindersCount binderTypesCount evs = runRepetitionGuardT maxTries $
+    spaceToDump r <$> initialiseSpace r bindersCount binderTypesCount evs
+
+-- |Creates an initial state
+initialiseSpace :: (MonadRandom m) =>
+     Int
+  -- ^Radius
+  -> Int
+  -- ^Number of binders
+  -> Int
+  -- ^Number of binders' types
+  -> [[EnergyVector]]
+  -- ^EnergyVectors of beads
+  -> RepetitionGuardT m Space
+  -- ^Initial state
+initialiseSpace r bindersCount bindersTypesCount beads = do
+    stateWithChains <- addChains r stateWithLamins beads
+    addBinders stateWithChains
+      where
+        stateWithLamins = addLamins M.empty $ spherePoints (fromIntegral r)
+        addBinders = foldr (<=<) pure . replicate bindersCount $ addBinder r bindersTypesCount
+
+-- |Converts Space to Dump
+spaceToDump :: Int -> Space -> Dump
+spaceToDump dumpRadius space = Dump{..}
+  where
+    dumpBinders = [Located l b | Located l (BinderSig b) <- M.elems space]
+    beads = [Located l b | Located l (BeadSig b) <- M.elems space]
+    dumpChains = map (map dropIndices) $
+        groupWith (view beadChain) $ sortWith (view beadAtomIndex) beads
+
+-- |Generates all points such that their distance from (0,0,0)
+-- is in the range [radius - 2, radius + 2]
+spherePoints :: Int -> [Vec3]
+spherePoints radius = [v | [x,y,z] <- replicateM 3 [-radius..radius],
+                      let v = V3 x y z,
+                      let q = quadrance v,
+                      q >= (radius - 2)^2 && q <= (radius + 2)^2]
+
+-- |Adds lamin binders into space
+addLamins :: Space -> [Vec3] -> Space
+addLamins space positions =
+    space `M.union` M.fromList [(v3, asAtom $ BinderInfo v3 laminType) | v3 <- positions]
+
+-- |Tries to add beads of one chain in the space
+addChain :: (MonadRandom m) =>
+    Int
+ -- ^Index of the chain we are placing
+ -> Int
+ -- ^Chain index of the first bead to be added
+ -> Int
+ -- ^Global index of the first atom to be added
+ -> Vec3
+ -- ^Starting point
+ -> Space
+ -- ^Current space
+ -> [EnergyVector]
+ -- ^Energy vectors of atoms that are going to be added
+ -> RepetitionGuardT m Space
+ -- ^The state with a addeded chain
+addChain _ _ _ _ space [] = return space
+addChain chainNo indOnChain glInd start space [ev] =
+    return $ M.insert start (asAtom $ BeadInfo start ev glInd chainNo indOnChain) space
+addChain chainNo indOnChain glInd start space (ev:evs) = hide $ do
+    nextRep
+    moves <- shuffleM $ V.toList legalMoves
+    let targets = [target | move <- moves,
+                            let target = move + start,
+                            not $ target `M.member` space,
+                            not $ intersectsChain space start target]
+    let newSpace = M.insert start (asAtom $ BeadInfo start ev glInd chainNo indOnChain) space
+    endings <- forM targets $
+      \m -> catch $ addChain chainNo (indOnChain + 1) (glInd + 1) m newSpace evs
+    return $ listToMaybe . catMaybes $ endings
+
+-- |Tries to find a free position
+getRandomFreePosition :: (MonadRandom m) => Int -> Space -> RepetitionGuardT m Vec3
+getRandomFreePosition r space = do
+    nextRep
+    [x, y, z] <- replicateM 3 $ getRandomR (-r, r)
+    let ans = V3 x y z
+    if M.member ans space || quadrance ans > r^2
+        then getRandomFreePosition r space else return ans
+
+-- |Addss a binder of random type in a space
+addBinder :: (MonadRandom m) =>
+    Int
+ -- ^Radius
+ -> Int
+ -- ^Number of binder types (excluding lamin),
+ -- must be a non-negative integer
+ -> Space
+ -- ^Current space
+ -> RepetitionGuardT m Space
+ -- ^Space with added binder
+addBinder r types space = do
+    pos <- getRandomFreePosition r space
+    binderType <- getRandomR (1, types)
+    return $ M.insert pos (asAtom $ BinderInfo pos $ BinderType binderType) space
+
+-- |Finds a good starting point for a chain and adds it there
+hookAndAddChain :: (MonadRandom m) =>
+    Int
+ -- ^Radius
+ -> Int
+ -- ^Chain number
+ -> Int
+ -- ^Global index of the first bead in the chain
+ -> [EnergyVector]
+ -- ^Energy vectors of beads in the chain
+ -> Space
+ -- ^Current space
+ -> RepetitionGuardT m Space
+hookAndAddChain r chainNo glInd vectors space = do
+    start <- getRandomFreePosition r space
+    result <- catch $ addChain chainNo 0 glInd start space vectors
+    maybe (hookAndAddChain r chainNo glInd vectors space) return result
+
+-- |Adds all the chains somewhere in the space
+addChains :: (MonadRandom m) =>
+    Int
+ -- ^Radius
+  -> Space
+ -- ^Current Space
+  -> [[EnergyVector]]
+ -- ^Energy vectors of the beads in the chains
+  -> RepetitionGuardT m Space
+addChains r space vectors =
+    fst <$> foldM (\(s, count) (evs, nr) -> (,count + length evs) <$>
+        hookAndAddChain r nr count evs s) (space, 0) (zip vectors [0..])
---
+++ test/StateInitialisationSpec.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE OverloadedLists #-}
+module StateInitialisationSpec where
+
+import Data.Maybe
+import Test.Hspec
+import Control.Lens
+import Control.Monad.Random.Class
+import Control.Monad.Random
+import qualified Data.Vector.Unboxed as U
+import qualified Data.Map as M
+
+import Bio.Motions.Common
+import Bio.Motions.StateInitialisation
+import Bio.Motions.Types
+import Bio.Motions.Representation.Dump
+import Bio.Motions.Representation.Common
+
+
+spec :: Spec
+spec = context "when generating state" $ do
+    let ans = run (initialise 10000 10 12 3 fewShortEVectors) 123
+    it "should generate something, when possible" $
+        ans `shouldSatisfy` isJust
+    it "should generate nothing, when impossible" $
+        run (initialise 1000 100 3 3 oneSuperLongEVector) 123 `shouldSatisfy` isNothing
+    let Just dump = ans
+    it "should generate proper amount of non-lamin binders" $
+        length (dumpBinders dump) - length (getAllLamins dump) `shouldBe` 12
+    it "should generate beads with proper energy vectors" $
+        map (map dumpBeadEV) (dumpChains dump) `shouldBe` fewShortEVectors
+    let ansSpace = dumpToSpace dump
+    let beadsPositions = map (map dumpBeadPosition) $ dumpChains dump
+    it "should generate non-intersecting chains" $
+        beadsPositions `shouldNotSatisfy` any (\chain ->
+            or $ zipWith (intersectsChain ansSpace) chain (tail chain))
+    it "should generate atoms on distinct fields" $
+        M.size ansSpace `shouldBe` length (concat $ dumpChains dump) + length (dumpBinders dump)
+
+run :: Rand StdGen (Maybe Dump) -> Int -> Maybe Dump
+run m seed = evalRand m $ mkStdGen seed
+
+getAllLamins :: Dump -> [BinderInfo]
+getAllLamins d = filter (\x -> x ^. binderType == laminType) $ dumpBinders d
+
+dumpToSpace :: Dump -> Space
+dumpToSpace dump = M.fromList $ [(b ^. position, asAtom b) | b <- dumpBinders dump] ++
+                                [(b ^. position, asAtom b) | b <- concat $ addIndices $ dumpChains dump]
+
+fewShortEVectors :: [[EnergyVector]]
+fewShortEVectors = [[[0,1], [1,0], [2,0], [1,0], [1,0]],
+                    [[0,0], [1,1], [1,1], [0,0]],
+                    [[0,1], [1,1], [1,1], [0,0]]]
+
+oneSuperLongEVector :: [[EnergyVector]]
+oneSuperLongEVector = [replicate 100000 [1,1,1]]
