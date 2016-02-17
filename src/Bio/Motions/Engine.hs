e254eb13d858dec85beb26c3ecc5cd5a83fcaadb
Kamil Braun <kb346840@students.mimuw.edu.pl>
2016-02-18 00:49:24 +0100

    Implement Main function.
    Change-Id: Iaf79c78f4dba31e5d2307480319740ce648724fc

--- src/Bio/Motions/Engine.hs
+++ src/Bio/Motions/Engine.hs
@@ -7,30 +7,53 @@ Portability : unportable
  -}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# OPTIONS_GHC -fno-warn-partial-type-signatures #-}
 module Bio.Motions.Engine where
 
 import Bio.Motions.Types
+import Bio.Motions.Common
 import Bio.Motions.Representation.Class
 import Bio.Motions.Callback.Class
-import Control.Applicative
+import Bio.Motions.PDB.Write
+import Bio.Motions.PDB.Meta
+import Bio.Motions.Representation.Dump
+import Bio.Motions.EnabledCallbacks
+
 import Control.Monad.State
 import Control.Monad.Random
 import Control.Monad.Trans.Maybe
+import System.IO
+import Control.Lens
 
--- |Contains the simulatiom state: a 'Representation' 'repr' and a 'Score' 'score'.
 data SimulationState repr score = SimulationState
     { repr :: repr
     , score :: score
+    , preCallbackResults :: [CallbackResult Pre]
+    , postCallbackResults :: [CallbackResult Post]
+    , stepCounter :: Int
+    , frameCounter :: Int
     }
 
--- |A simple implementation (just to be sure it typechecks). TODO: rewrite.
--- As a dirty hack, it requires the 'score' to be 'Integral'.
-simulateStep :: (Monad m, Alternative m, Representation m repr, Integral score,
-    MonadRandom m, Score m score, MonadState (SimulationState repr score) m) => m Move
-simulateStep = do
-    SimulationState{..} <- get
+-- |Describes how the simulation should run.
+data RunSettings repr score = RunSettings
+    { pdbFile :: FilePath
+    -- ^ Path to the PDB output file.
+    , numSteps :: Int
+    -- ^ Number of simulation steps.
+    , writeIntermediatePDB :: Bool
+    -- ^ Whether to write intermediate PDB frames.
+    }
+
+step :: (MonadRandom m, MonadState (SimulationState repr score) m,
+         Representation (MaybeT m) repr, Score score) => m (Maybe Move)
+step = runMaybeT $ do
+    st@SimulationState{..} <- get
     move <- generateMove repr
     newScore <- updateCallback repr score move
+    newPreCallbackResults <- mapM (updateCallbackResult repr move) preCallbackResults
 
     let delta = fromIntegral $ newScore - score
     unless (delta >= 0) $ do
@@ -38,9 +61,76 @@ simulateStep = do
         guard $ r < exp (delta * factor)
 
     (newRepr, _) <- performMove move repr
-    put SimulationState { repr = newRepr, score = newScore }
+    newPostCallbackResults <- mapM (updateCallbackResult newRepr move) postCallbackResults
+
+    put st { repr = newRepr
+           , score = newScore
+           , preCallbackResults = newPreCallbackResults
+           , postCallbackResults = newPostCallbackResults
+           }
 
     pure move
   where
     factor :: Double
     factor = 2
+
+pushPDB :: _ => Handle -> m ()
+pushPDB handle = do
+    st@SimulationState{..} <- get
+    dump <- removeLamins <$> makeDump repr -- TODO: remove lamins?
+
+    let frameHeader = FrameHeader { headerSeqNum = frameCounter
+                                  , headerStep = stepCounter
+                                  , headerTitle = "chromosome;bonds=" ++ show score
+                                  }
+
+    liftIO $ writePDB handle frameHeader legacyPDBMeta dump >> hPutStrLn handle "END"
+
+    put st { frameCounter = frameCounter + 1 }
+  where
+    removeLamins d = d { dumpBinders = filter notLamin $ dumpBinders d }
+    notLamin b = b ^. binderType /= laminType
+
+stepAndWrite :: _ => Handle -> Maybe Handle -> m ()
+stepAndWrite callbacksHandle pdbHandle = do
+    oldScore <- gets score
+    step -- TODO: do something with the move
+    newScore <- gets score
+
+    when (oldScore /= newScore) $ do
+        writeCallbacks callbacksHandle
+        case pdbHandle of
+          Just handle -> pushPDB handle
+          Nothing -> pure ()
+
+    modify $ \s -> s { stepCounter = stepCounter s + 1 }
+
+writeCallbacks :: _ => Handle -> m ()
+writeCallbacks handle = do
+    gets preCallbackResults >>= mapM_ writeCallbackResult
+    gets postCallbackResults >>= mapM_ writeCallbackResult
+  where
+    writeCallbackResult (CallbackResult cb) = liftIO $ hPutStrLn handle $ getCallbackName cb ++ ": " ++ show cb
+
+simulate :: _ => RunSettings repr score -> Dump -> m Dump
+simulate (RunSettings{..} :: RunSettings repr score) dump = do
+    repr :: repr <- loadDump dump
+    score :: score <- runCallback repr
+
+    preCallbackResults <- getCallbackResults repr enabledPreCallbacks
+    postCallbackResults <- getCallbackResults repr enabledPostCallbacks
+    let stepCounter = 0
+        frameCounter = 0
+        writeNextFrame = True
+        st = SimulationState{..}
+
+    let callbacksHandle = stdout
+    pdbHandle <- liftIO $ openFile pdbFile WriteMode
+    st' <- flip execStateT st $ do
+        when writeIntermediatePDB $ pushPDB pdbHandle
+        replicateM_ numSteps $ stepAndWrite callbacksHandle $ guard writeIntermediatePDB >> Just pdbHandle
+        unless writeIntermediatePDB $ pushPDB pdbHandle
+    liftIO $ hClose pdbHandle
+    let SimulationState{..} = st'
+
+    makeDump repr
