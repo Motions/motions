dce96ecd07bfcc7dce40a33fa113b80afc81be32
Kamil Braun <kb346840@students.mimuw.edu.pl>
2016-02-28 12:59:23 +0100

    Add FFI bindings to libprotostream.
    Change-Id: Ide5d574eecb3008e2df7661e66326a81dde1a54a

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,62 @@
+import Distribution.PackageDescription
 import Distribution.Simple
-main = defaultMain
+import Distribution.Simple.LocalBuildInfo
+import Distribution.Simple.Setup
+import Distribution.Simple.Utils
+
+import System.Directory
+import System.FilePath
+import Data.Maybe
+import Control.Monad
+
+main = defaultMainWithHooks simpleUserHooks
+    { preConf = makeExtLib
+    , confHook = \a f -> confHook simpleUserHooks a f >>= updateExtraLibDirs
+    }
+
+makeExtLib :: Args -> ConfigFlags -> IO HookedBuildInfo
+makeExtLib _ flags = do
+    let verbosity = fromFlag $ configVerbosity flags
+    dir <- joinPath . (: ["libprotostream"]) <$> getCurrentDirectory
+
+    dirExists <- doesDirectoryExist dir
+    unless dirExists $
+        rawSystemExit verbosity "env"
+            ["git", "clone", "https://github.com/Motions/libprotostream.git", dir]
+    withCurrentDirectory dir $ do
+        rawSystemExit verbosity "env"
+            ["git", "checkout", "master"] --TODO: smarter
+        rawSystemExit verbosity "env"
+            ["git", "pull"]
+
+    let buildDir = joinPath . (: ["build"]) $ dir
+    createDirectoryIfMissing False buildDir
+    withCurrentDirectory buildDir $ do
+        rawSystemExit verbosity "env" ["cmake", ".."]
+        rawSystemExit verbosity "env" ["make", "cprotostream_static"]
+
+    return emptyHookedBuildInfo
+
+updateExtraLibDirs :: LocalBuildInfo -> IO LocalBuildInfo
+updateExtraLibDirs localBuildInfo = do
+    let packageDescription = localPkgDescr localBuildInfo
+        lib = fromJust $ library packageDescription
+        libBuild = libBuildInfo lib
+    dir <- joinPath . (: ["libprotostream", "src"]) <$> getCurrentDirectory
+    return localBuildInfo {
+        localPkgDescr = packageDescription {
+            library = Just $ lib {
+                libBuildInfo = libBuild {
+                    extraLibDirs = dir : extraLibDirs libBuild
+                }
+            }
+        }
+    }
+
+withCurrentDirectory :: FilePath -> IO a -> IO a
+withCurrentDirectory path act = do
+    oldDir <- getCurrentDirectory
+    setCurrentDirectory path
+    ret <- act
+    setCurrentDirectory oldDir
+    pure ret
