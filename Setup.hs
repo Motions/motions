1d0db1bf698e79d708085b7399fb50abcf74e2b1
Rafał Stefański <ravst94@gmail.com>
2016-03-12 01:28:50 +0000

    Dump to proto serialisation and from proto deserialisation
    Change-Id: I03c096fade9889f4f2bd06d33ef11dc6502a3858

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,59 @@
 import Distribution.Simple
-main = defaultMain
+import Distribution.Simple.Setup
+import Distribution.PackageDescription
+import Distribution.Simple.Utils
+import Distribution.Verbosity
+import System.Process
+import System.Exit
+import System.FilePath
+import System.Directory
+import Control.Monad
+
+downloadProtoDescription :: Verbosity -> IO ()
+downloadProtoDescription verbosity =
+    rawSystemExit verbosity "env"
+        ["curl", "https://raw.githubusercontent.com/Motions/Format/master/message.proto",
+        "-o", "src/message.proto"]
+
+compileProtos :: Verbosity -> IO ()
+compileProtos verbosity = do
+    dir <- joinPath . (: ["src"]) <$> getCurrentDirectory
+    withCurrentDirectory dir $
+        rawSystemExit verbosity "env" ["hprotoc", "message.proto"]
+
+handleProtos :: Verbosity -> IO ()
+handleProtos verbosity = do
+    downloadProtoDescription verbosity
+    compileProtos verbosity
+
+removeFileIfExists :: FilePath -> IO ()
+removeFileIfExists fileName = do
+    fileExist <- doesFileExist fileName
+    when fileExist $ removeFile fileName
+
+removeDirectoryIfExists :: FilePath -> IO ()
+removeDirectoryIfExists fileName = do
+    fileExists <- doesDirectoryExist fileName
+    when fileExists $ removeDirectoryRecursive fileName
+
+deleteCompiledProtos :: IO ()
+deleteCompiledProtos = do
+    removeDirectoryIfExists "src/Bio/Motions/Format/Proto"
+    removeFileIfExists "src/Bio/Motions/Format/Proto.hs"
+
+makePreHandle :: (Verbosity -> IO ()) -> a -> ConfigFlags -> IO HookedBuildInfo
+makePreHandle function _  flags  = do
+    function $ fromFlag $ configVerbosity flags
+    return emptyHookedBuildInfo
+
+withCurrentDirectory :: FilePath -> IO a -> IO a
+withCurrentDirectory path act = do
+    oldDir <- getCurrentDirectory
+    setCurrentDirectory path
+    ret <- act
+    setCurrentDirectory oldDir
+    return ret
+
+main = defaultMainWithHooks
+        simpleUserHooks {preConf = makePreHandle handleProtos
+                        ,preClean = (\_ _ -> deleteCompiledProtos >> return emptyHookedBuildInfo)}
