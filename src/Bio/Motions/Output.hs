cc380804a81f0d5be3186c9b9e96f23ddee3ea22
Bartosz Stebel <bartoszstebel@gmail.com>
2016-03-23 23:09:08 +0100

    WIP: support multiple output backends
    Change-Id: I4a4c92c40b01d00827cb18b62d1598c4617f005f

---
+++ src/Bio/Motions/Output.hs
@@ -0,0 +1,18 @@
+module Bio.Motions.Output where
+
+import Bio.Motions.Types
+import Bio.Motions.Representation.Dump
+
+data OutputSettings = OutputSettings
+    { outputPrefix :: FilePath
+    , simulationName :: String
+    , simulationDescription :: String
+    , chainNames :: [String]
+    }
+
+class OutputBackend a where
+    getNextPush :: a -> IO PushOutputFrame
+    bClose :: a -> IO ()
+
+-- TODO PushMove should do callbacks
+data PushOutputFrame = PushDump (Dump -> IO ()) | PushMove (Move -> IO ()) | DoNothing
