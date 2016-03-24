44076d117e4e0419007d33a0c2ea8a5c5c5aa155
Krzysztof Pszeniczny <krzysztof.pszeniczny@gmail.com>
2016-03-24 12:35:28 +0100

    Add periodic callbacks
    Only the "every N accepted frames" callbacks are supported for now,
    as opposed to the "every N frames" callbacks.
    
    Change-Id: I8af3a48f132583008b0b8b474743802f572f074c

---
+++ src/Bio/Motions/Callback/Periodic.hs
@@ -0,0 +1,58 @@
+{- |
+Module      : Bio.Motions.Callback.Periodic
+Description : Contains the Periodic callback transformer
+License     : Apache
+Stability   : experimental
+Portability : unportable
+ -}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+module Bio.Motions.Callback.Periodic
+    ( CallbackPeriod
+    , Periodic(..)
+    ) where
+
+import Bio.Motions.Callback.Class
+import Data.Proxy
+import GHC.TypeLits
+
+-- |The interval between successive recomputations of a 'Callback' 'cb', when transformed
+-- using the 'Periodic' callback transformer
+type family CallbackPeriod cb :: Nat
+
+-- |Transforms a 'Callback' 'cb' into a 'Callback' which is run every 'CallbackPeriod cb' steps.
+data Periodic cb = PeriodicValue { periodicValue :: !cb }
+                 -- ^The value of 'cb' is present right now
+                 | PeriodicWait { periodicWait :: !Int }
+                 -- ^The value will be computed in 'periodicWait' steps.
+                 deriving Eq
+
+instance Show cb => Show (Periodic cb) where
+    show (PeriodicValue v) = show v
+    show _ = "---"
+
+-- | Note: The leading '_' is dropped from the base callback's name, if present.
+instance (Callback mode cb, KnownNat (CallbackPeriod cb), CmpNat 0 (CallbackPeriod cb) ~ 'LT)
+          => Callback mode (Periodic cb) where
+    callbackName _ = case callbackName (Proxy :: Proxy cb) of
+        '_' : name -> name
+        name -> name
+    {-# INLINEABLE callbackName #-}
+
+    runCallback = fmap PeriodicValue . runCallback
+    {-# INLINE runCallback #-}
+
+    -- |Updates the value of the callback only once in 'CallbackPeriod cb' steps.
+    updateCallback repr (PeriodicWait 1) _ = runCallback repr
+    updateCallback _ (PeriodicWait n) _ = pure . PeriodicWait $ n - 1
+    updateCallback repr (PeriodicValue cb) move
+        | period == 1 = PeriodicValue <$> updateCallback repr cb move
+        | otherwise = pure $ PeriodicWait period
+        where period = fromInteger $ natVal (Proxy :: Proxy (CallbackPeriod cb))
+    {-# INLINEABLE updateCallback #-}
