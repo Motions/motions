17512dc43de5aadd894d7d12bc2e12665cdb501e
Krzysztof Pszeniczny <krzysztof.pszeniczny@gmail.com>
2016-03-24 12:35:28 +0100

    Add periodic callbacks
    Only the "every N accepted frames" callbacks are supported for now,
    as opposed to the "every N frames" callbacks.
    
    Change-Id: I8af3a48f132583008b0b8b474743802f572f074c

--- test/LoadTestCallbacks.hs
+++ test/LoadTestCallbacks.hs
@@ -7,43 +7,56 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE StandaloneDeriving #-}
 module LoadTestCallbacks where
 
+import Bio.Motions.Callback.Class
 import Bio.Motions.Callback.Parser.TH
+import Bio.Motions.Callback.Periodic
 
 [callback|CALLBACK "sum42-beads"
-    EVERY 1
+    EVERY ACCEPTED 1
     NODES 1
     WHERE BELONGS(X 0, BEAD_BINDING_TO 0) OR BELONGS(X 0, BEAD_BINDING_TO 1)
     COMPUTE SUM 42
 |]
 
 [callback|CALLBACK "prod2-all"
-    EVERY 1
+    EVERY ACCEPTED 1
     NODES 1
     WHERE 1 == 1
     COMPUTE PRODUCT 2
 |]
 
 [callback|CALLBACK "list42-binders"
-    EVERY 1
+    EVERY ACCEPTED 1
     NODES 1
     WHERE BELONGS(X 0, BINDER 0) OR BELONGS(X 0, BINDER 1)
     COMPUTE LIST 42
 |]
 
 [callback|CALLBACK "pairs-dist<2"
-    EVERY 1
+    EVERY ACCEPTED 1
     NODES 2
     WHERE DIST(X 0, X 1) * DIST(X 0, X 1) < 2
     COMPUTE SUM 1
 |]
 
 [callback|CALLBACK "complex-function"
-    EVERY 1
+    EVERY ACCEPTED 1
     NODES 5
     WHERE BELONGS(X 0, BINDER 1) AND BELONGS(X 4, BEAD_BINDING_TO 0) AND DIST(X 1, X 3) <= 1.5
     COMPUTE SUM DIST(X 0, X 1) + DIST(X 1, X 4) * DIST(X 3, X 2) - DIST(X 4, X 0)
 |]
 
 [callbacksFile|test/testdata/callbacks|]
+
+data EmptyCallback
+deriving instance Show EmptyCallback
+
+instance Callback 'Pre EmptyCallback where
+    callbackName _ = "_empty"
+    runCallback = undefined
+    updateCallback = undefined
+
+type instance CallbackPeriod EmptyCallback = 3
