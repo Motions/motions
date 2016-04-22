5826e4ce02c06a115c96c77c694e275b3ec86a15
Krzysztof Pszeniczny <krzysztof.pszeniczny@gmail.com>
2016-04-22 23:00:50 +0200

    WIP: Move backend selection to Main, cleanup
    Change-Id: I7d1cce9d1d8c7ccc732b4921e818e82e964d6640

---
+++ Specialise.hs
@@ -0,0 +1,25 @@
+{- |
+Module      : Specialise
+Description : Contains specialisations of simulate for commonly used parameters
+License     : Apache
+Stability   : experimental
+Portability : unportable
+ -}
+module Specialise where
+
+import Bio.Motions.Callback.StandardScore
+import qualified Bio.Motions.Engine as E
+import Bio.Motions.Format.Handle
+import Bio.Motions.PDB.Backend
+import Bio.Motions.Representation.Chain
+import Bio.Motions.Representation.Dump
+import Bio.Motions.Utils.Random
+import GHC.Exts
+
+simulate'IOChain'StandardScore'PDB'MWCIO :: E.RunSettings IOChainRepresentation StandardScore PDBBackend -> Dump -> MWCIO Dump
+simulate'IOChain'StandardScore'PDB'MWCIO = inline E.simulate
+{-# NOINLINE simulate'IOChain'StandardScore'PDB'MWCIO #-}
+
+simulate'IOChain'StandardScore'Bin'MWCIO :: E.RunSettings IOChainRepresentation StandardScore BinaryBackend -> Dump -> MWCIO Dump
+simulate'IOChain'StandardScore'Bin'MWCIO = inline E.simulate
+{-# NOINLINE simulate'IOChain'StandardScore'Bin'MWCIO #-}
