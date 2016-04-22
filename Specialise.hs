368e8232527b1c4d4f2b4722f0f7dd7d9fc2bd0c
Krzysztof Pszeniczny <krzysztof.pszeniczny@gmail.com>
2016-04-22 23:00:50 +0200

    Move backend selection to Main, cleanup
    Change-Id: I7d1cce9d1d8c7ccc732b4921e818e82e964d6640

---
+++ Specialise.hs
@@ -0,0 +1,34 @@
+{- |
+Module      : Specialise
+Description : Contains specialisations of simulate for commonly used parameters
+License     : Apache
+Stability   : experimental
+Portability : unportable
+ -}
+module Specialise where
+
+import Bio.Motions.Callback.StandardScore
+import Bio.Motions.Engine
+import Bio.Motions.Format.Handle
+import Bio.Motions.PDB.Backend
+import Bio.Motions.Representation.Chain
+import Bio.Motions.Representation.Dump
+import Bio.Motions.Utils.Random
+import GHC.Exts
+
+{- We want to force the GHC to specialise the 'simulate' function for common sets of parameters,
+ - vastly improving performance. However, it seems that SPECIALISE pragmas alone are not sufficient,
+ - GHC ignores them in the "Main" module, despite the specialisable code being present in the generated
+ - Core. As a workaround, we use explicit rewrite rules to replace this code with its specialisation.
+ - However, those RULES would also apply to the definition of the specialised function. Therefore,
+ - we define the specialisations in a separate module.
+ -}
+simulate'IOChain'StandardScore'PDB'MWCIO :: RunSettings IOChainRepresentation StandardScore PDBBackend
+                                            -> Dump -> MWCIO Dump
+simulate'IOChain'StandardScore'PDB'MWCIO = inline simulate
+{-# NOINLINE simulate'IOChain'StandardScore'PDB'MWCIO #-}
+
+simulate'IOChain'StandardScore'Bin'MWCIO :: RunSettings IOChainRepresentation StandardScore BinaryBackend
+                                            -> Dump -> MWCIO Dump
+simulate'IOChain'StandardScore'Bin'MWCIO = inline simulate
+{-# NOINLINE simulate'IOChain'StandardScore'Bin'MWCIO #-}
