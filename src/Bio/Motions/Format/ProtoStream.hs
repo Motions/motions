a294947859743289f0ba5cd93ea307f5588a1d04
Krzysztof Pszeniczny <krzysztof.pszeniczny@gmail.com>
2016-04-23 15:31:04 +0200

    Mark bindings to cprotostream as unsafe
    Change-Id: Ia88002612482003c4125169bd5ba0ce000d0869f

--- src/Bio/Motions/Format/ProtoStream.hs
+++ src/Bio/Motions/Format/ProtoStream.hs
@@ -16,59 +16,59 @@ data HStream
 data HKeyframeIterator
 data HDeltaIterator
 
-foreign import ccall "protostream_open_existing"
+foreign import ccall unsafe "protostream_open_existing"
     protoOpenExisting :: CString -> IO (Ptr HStream)
 
-foreign import ccall "protostream_open_new"
+foreign import ccall unsafe "protostream_open_new"
     protoOpenNew :: CString -> CUInt -> Ptr () -> CSize -> IO (Ptr HStream)
 
-foreign import ccall "protostream_close"
+foreign import ccall unsafe "protostream_close"
     protoClose :: Ptr HStream -> IO ()
 
-foreign import ccall "protostream_append_keyframe"
+foreign import ccall unsafe "protostream_append_keyframe"
     protoAppendKeyframe :: Ptr HStream -> Ptr () -> CSize -> IO ()
 
-foreign import ccall "protostream_append_delta"
+foreign import ccall unsafe "protostream_append_delta"
     protoAppendDelta :: Ptr HStream -> Ptr () -> CSize -> IO ()
 
-foreign import ccall "protostream_get_header"
+foreign import ccall unsafe "protostream_get_header"
     protoGetHeader :: Ptr HStream -> Ptr (Ptr ()) -> Ptr CSize -> IO ()
 
-foreign import ccall "protostream_free_header"
+foreign import ccall unsafe "protostream_free_header"
     protoFreeHeader :: Ptr () -> IO ()
 
-foreign import ccall "protostream_iter_keyframes"
+foreign import ccall unsafe "protostream_iter_keyframes"
     protoIterKeyframes :: Ptr HStream -> IO (Ptr HKeyframeIterator)
 
-foreign import ccall "protostream_get_keyframe"
+foreign import ccall unsafe "protostream_get_keyframe"
     protoGetKeyframe :: Ptr HKeyframeIterator -> Ptr (Ptr ()) -> Ptr CSize -> IO ()
 
-foreign import ccall "protostream_free_keyframe"
+foreign import ccall unsafe "protostream_free_keyframe"
     protoFreeKeyframe :: Ptr () -> IO ()
 
-foreign import ccall "protostream_valid_keyframe_iterator"
+foreign import ccall unsafe "protostream_valid_keyframe_iterator"
     protoValidKeyframeIterator :: Ptr HStream -> Ptr HKeyframeIterator -> IO CInt
 
-foreign import ccall "protostream_advance_keyframe_iterator"
+foreign import ccall unsafe "protostream_advance_keyframe_iterator"
     protoAdvanceKeyframeIterator :: Ptr HKeyframeIterator -> IO ()
 
-foreign import ccall "protostream_free_keyframe_iterator"
+foreign import ccall unsafe "protostream_free_keyframe_iterator"
     protoFreeKeyframeIterator :: Ptr HKeyframeIterator -> IO ()
 
-foreign import ccall "protostream_iter_deltas"
+foreign import ccall unsafe "protostream_iter_deltas"
     protoIterDeltas :: Ptr HKeyframeIterator -> IO (Ptr HDeltaIterator)
 
-foreign import ccall "protostream_get_delta"
+foreign import ccall unsafe "protostream_get_delta"
     protoGetDelta :: Ptr HDeltaIterator -> Ptr (Ptr ()) -> Ptr CSize -> IO ()
 
-foreign import ccall "protostream_free_delta"
+foreign import ccall unsafe "protostream_free_delta"
     protoFreeDelta :: Ptr () -> IO ()
 
-foreign import ccall "protostream_valid_delta_iterator"
+foreign import ccall unsafe "protostream_valid_delta_iterator"
     protoValidDeltaIterator :: Ptr HKeyframeIterator -> Ptr HDeltaIterator -> IO CInt
 
-foreign import ccall "protostream_advance_delta_iterator"
+foreign import ccall unsafe "protostream_advance_delta_iterator"
     protoAdvanceDeltaIterator :: Ptr HDeltaIterator -> IO ()
 
-foreign import ccall "protostream_free_delta_iterator"
+foreign import ccall unsafe "protostream_free_delta_iterator"
     protoFreeDeltaIterator :: Ptr HDeltaIterator -> IO ()
