23c7cbdbff3ca0b5195b8db98eac9f65aef40f71
Kamil Braun <kb346840@students.mimuw.edu.pl>
2016-05-08 01:14:26 +0200

    Implement a dump difference function.
    Change-Id: I76471a483f45fd5432f08e205c62f0f64481cc50

--- src/Bio/Motions/Representation/Dump.hs
+++ src/Bio/Motions/Representation/Dump.hs
@@ -9,6 +9,7 @@ Portability : unportable
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE LambdaCase #-}
 module Bio.Motions.Representation.Dump where
 
 import Bio.Motions.Common
@@ -16,6 +17,7 @@ import Bio.Motions.Types
 import Control.Lens
 import Control.Monad
 import Control.Monad.State.Strict
+import Control.Monad.Except
 import GHC.Generics (Generic)
 import Control.DeepSeq
 
@@ -60,3 +62,71 @@ dropIndices b = DumpBeadInfo
     { dumpBeadPosition = b ^. position
     , dumpBeadEV = b ^. beadEV
     }
+
+type DiffFinder = StateT (Maybe Move) (Except String) ()
+
+-- |Performs a difference of two dumps, searching for a single position change.
+--  Returns an error if no difference, multiple differences, or any other inconsistencies were found.
+--  Otherwise returns the resulting 'Move'.
+--  Assumes that both dumps are correct.
+diffDumps ::
+     Dump
+  -- ^The dump from which the move was performed.
+  -> Dump
+  -- ^The dump after the move was performed.
+  -> Either String Move
+  -- ^The resulting move or error.
+diffDumps d1 d2 =
+    case runExcept (execStateT diff Nothing) of
+      Left err       -> Left err
+      Right (Just m) -> Right m
+      Right _        -> Left "No differences found"
+  where
+    diff :: DiffFinder
+    diff = diffBinders >> diffChains
+
+    diffBinders :: DiffFinder
+    diffBinders = do
+        let bs1 = dumpBinders d1
+            bs2 = dumpBinders d2
+        when (length bs1 /= length bs2)
+            $ throwError "Different binder list lengths"
+        -- TODO: write tests for ordered binders
+        zipWithM_ diffTwoBinders bs1 bs2
+
+    diffChains :: DiffFinder
+    diffChains = do
+        let chs1 = dumpChains d1
+            chs2 = dumpChains d2
+        when (length chs1 /= length chs2)
+            $ throwError $ "Different chain counts: " ++ show (length chs1) ++ ", " ++ show (length chs2)
+        zipWithM_ diffTwoChains chs1 chs2
+
+    diffTwoChains :: [DumpBeadInfo] -> [DumpBeadInfo] -> DiffFinder
+    diffTwoChains ch1 ch2 = do
+        when (length ch1 /= length ch2)
+            $ throwError $ "Different chain lengths: " ++ show (ch1, length ch1)
+                                               ++ ", " ++ show (ch2, length ch2)
+        zipWithM_ diffTwoBeads ch1 ch2
+
+    diffTwoBeads :: DumpBeadInfo -> DumpBeadInfo -> DiffFinder
+    diffTwoBeads b1 b2 = do
+        when (dumpBeadEV b1 /= dumpBeadEV b2)
+            $ throwError $ "Beads with the same index but different EVs: " ++ show b1 ++ ", " ++ show b2
+        diffPositions (dumpBeadPosition b1) (dumpBeadPosition b2)
+
+    diffTwoBinders :: BinderInfo -> BinderInfo -> DiffFinder
+    diffTwoBinders b1 b2 = do
+        when (b1 ^. binderType /= b2 ^. binderType)
+            $ throwError $ "Binders with the same index but different types: " ++ show b1 ++ ", " ++ show b2
+        when (b1 ^. binderType == laminType && b1 ^. position /= b2 ^. position)
+            $ throwError $ "A lamin binder has moved: " ++ show b1 ++ ", " ++ show b2
+        diffPositions (b1 ^. position) (b2 ^. position)
+
+    diffPositions :: Vec3 -> Vec3 -> DiffFinder
+    diffPositions v1 v2 = unless (v1 == v2) $ recordMove $ MoveFromTo v1 v2
+
+    recordMove :: Move -> DiffFinder
+    recordMove m = get >>= \case
+        Nothing  -> put $ Just m
+        Just m'  -> throwError $ "At least two differences occured: " ++ show m ++ ", " ++ show m'
--- test/DumpSpec.hs
+++ test/DumpSpec.hs
@@ -5,6 +5,8 @@ import Test.Hspec
 
 import Bio.Motions.Representation.Dump
 import Bio.Motions.Types
+import Bio.Motions.Common
+import Data.Either
 import Linear
 
 testDump :: Spec
@@ -36,5 +38,129 @@ testDump =
         ]
     (ev0, ev1) = ([1, 2], [0, 2])
 
+testDumpDiff :: Spec
+testDumpDiff =
+    context "when computing Dump difference" $ do
+        it "computes a correct binder difference" $
+            diffDumps dump1 dump2 `shouldBe` Right (MoveFromTo (V3 1 1 1) (V3 1 1 2))
+        it "computes a correct bead difference" $
+            diffDumps dump1 dump2' `shouldBe` Right (MoveFromTo (V3 1 0 0) (V3 1 1 0))
+        it "fails when no difference was found" $
+            diffDumps dump1 dump1 `shouldSatisfy` isLeft
+        it "fails when a lamin binder has moved" $
+            diffDumps dump1 dump1MovedLamin `shouldSatisfy` isLeft
+        it "fails when chain counts don't match" $
+            diffDumps dump1 dump2ExtraChain `shouldSatisfy` isLeft
+        it "fails when corresponding chains have different length" $
+            diffDumps dump1 dump2ExtraBead `shouldSatisfy` isLeft
+        it "fails when corresponding beads have different EVs" $
+            diffDumps dump1 dump2ModifiedEV `shouldSatisfy` isLeft
+        it "fails when corresponding binders have different types" $
+            diffDumps dump1 dump2ModifiedBT `shouldSatisfy` isLeft
+        it "fails when two differences were found" $
+            diffDumps dump1 dump2ExtraDiff `shouldSatisfy` isLeft
+  where
+    dump1 :: Dump
+    dump1 = Dump dump1Binders dump1Chains
+
+    dump2 :: Dump
+    dump2 = Dump dump2Binders dump1Chains
+
+    dump2' :: Dump
+    dump2' = Dump dump1Binders dump2'Chains
+
+    dump1MovedLamin :: Dump
+    dump1MovedLamin = Dump dump1BindersMovedLamin dump1Chains
+
+    dump2ExtraChain :: Dump
+    dump2ExtraChain = Dump dump2Binders dump1ChainsExtraChain
+
+    dump2ExtraBead :: Dump
+    dump2ExtraBead = Dump dump2Binders dump1ChainsExtraBead
+
+    dump2ModifiedEV :: Dump
+    dump2ModifiedEV = Dump dump2Binders dump1ChainsModifiedEV
+
+    dump2ModifiedBT :: Dump
+    dump2ModifiedBT = Dump dump2BindersModifiedBT dump1Chains
+
+    dump2ExtraDiff :: Dump
+    dump2ExtraDiff = Dump  dump2Binders dump2'Chains
+
+    dump1Binders :: [BinderInfo]
+    dump1Binders =
+        [ BinderInfo (V3 0 1 0) bi0
+        , BinderInfo (V3 1 1 1) bi1
+        ]
+
+    dump2Binders :: [BinderInfo]
+    dump2Binders =
+        [ BinderInfo (V3 0 1 0) bi0
+        , BinderInfo (V3 1 1 2) bi1
+        ]
+
+    dump1Chains :: [[DumpBeadInfo]]
+    dump1Chains =
+        [ [ DumpBeadInfo (V3 0 0 0) ev0
+          , DumpBeadInfo (V3 0 0 1) ev1
+          ]
+        , [ DumpBeadInfo (V3 1 0 0) ev0
+          , DumpBeadInfo (V3 2 0 0) ev0
+          , DumpBeadInfo (V3 3 1 0) ev0
+          ]
+        ]
+
+    dump2'Chains :: [[DumpBeadInfo]]
+    dump2'Chains =
+        [ [ DumpBeadInfo (V3 0 0 0) ev0
+          , DumpBeadInfo (V3 0 0 1) ev1
+          ]
+        , [ DumpBeadInfo (V3 1 1 0) ev0
+          , DumpBeadInfo (V3 2 0 0) ev0
+          , DumpBeadInfo (V3 3 1 0) ev0
+          ]
+        ]
+
+    dump1BindersMovedLamin :: [BinderInfo]
+    dump1BindersMovedLamin =
+        [ BinderInfo (V3 0 2 0) bi0
+        , BinderInfo (V3 1 1 1) bi1
+        ]
+
+    dump1ChainsExtraChain :: [[DumpBeadInfo]]
+    dump1ChainsExtraChain = dump1Chains ++ [[ DumpBeadInfo (V3 4 0 0) ev0 ]]
+
+    dump1ChainsExtraBead :: [[DumpBeadInfo]]
+    dump1ChainsExtraBead =
+        [ [ DumpBeadInfo (V3 0 0 0) ev0
+          , DumpBeadInfo (V3 0 0 1) ev1
+          ]
+        , [ DumpBeadInfo (V3 1 0 0) ev0
+          , DumpBeadInfo (V3 2 0 0) ev0
+          , DumpBeadInfo (V3 3 1 0) ev0
+          , DumpBeadInfo (V3 4 0 0) ev0
+          ]
+        ]
+
+    dump1ChainsModifiedEV :: [[DumpBeadInfo]]
+    dump1ChainsModifiedEV =
+        [ [ DumpBeadInfo (V3 0 0 0) ev0
+          , DumpBeadInfo (V3 0 0 1) ev1
+          ]
+        , [ DumpBeadInfo (V3 1 0 0) ev0
+          , DumpBeadInfo (V3 2 0 0) ev1
+          , DumpBeadInfo (V3 3 1 0) ev0
+          ]
+        ]
+
+    dump2BindersModifiedBT :: [BinderInfo]
+    dump2BindersModifiedBT =
+        [ BinderInfo (V3 0 1 0) bi1
+        , BinderInfo (V3 1 1 2) bi1
+        ]
+
+    (bi0, bi1) = (laminType, BinderType 1)
+    (ev0, ev1) = ([0,0], [0,1])
+
 spec :: Spec
-spec = context "the Dump module" testDump
+spec = context "the Dump module" $ testDump >> testDumpDiff
