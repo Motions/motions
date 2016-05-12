45a7d3d76fe452ee25c40fa1a492b497f0ef18bd
Rafał Stefański <ravst94@gmail.com>
2016-05-12 02:33:05 +0100

    Chromosomes names no longer have to match (chr)?[0-9]*
    Change-Id: I54d231b5ba8827c133a4a067b23e3a9ff3f1c8af

--- src/Bio/Motions/Utils/Parsec.hs
+++ src/Bio/Motions/Utils/Parsec.hs
@@ -5,6 +5,7 @@ License     : Apache
 Stability   : experimental
 Portability : unportable
 -}
+{-# LANGUAGE FlexibleContexts #-}
 module Bio.Motions.Utils.Parsec where
 
 import Bio.Motions.Types
@@ -13,11 +14,14 @@ import Text.Parsec
 import Text.Parsec.ByteString
 import GHC.Exts
 
+word :: Stream s m Char => ParsecT s u m String
+word = manyTill anyChar (lookAhead space)
+
 energyVector :: Parser EnergyVector
 energyVector = fromList <$> listOf int
 
-listOf :: Parser a -> Parser [a]
+listOf :: Stream s m Char => ParsecT s u m a -> ParsecT s u m [a]
 listOf p = char '[' *> p `sepBy1` (spaces *> char ',') <* char ']'
 
-int :: Parser Int
+int :: Stream s m Char => ParsecT s u m Int
 int = read <$> many1 digit <?> "non-negative integer"
