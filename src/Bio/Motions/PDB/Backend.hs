39e887fd1738bc27fbe2647669a6de7c47119b99
Bartosz Stebel <bartoszstebel@gmail.com>
2016-06-12 22:19:03 +0200

    Input backends and MoveProducer
    This also fixes a bug in pdb output - first frame was never written
    
    Change-Id: I73380161714caf519ffda6f30b1e440fafdb5554

--- src/Bio/Motions/PDB/Backend.hs
+++ src/Bio/Motions/PDB/Backend.hs
@@ -6,20 +6,30 @@ Stability   : experimental
 Portability : unportable
 -}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 module Bio.Motions.PDB.Backend where
 
 import Bio.Motions.Common
 import Bio.Motions.Output
+import Bio.Motions.Input
 import Bio.Motions.Types
 import Bio.Motions.PDB.Write
+import Bio.Motions.PDB.Read
+import Bio.Motions.PDB.Internal(RevPDBMeta)
 import Bio.Motions.PDB.Meta
 import Bio.Motions.Representation.Dump
+import Bio.Motions.Representation.Class
+import Bio.Motions.Callback.Class
 
 import Control.Lens
 import Control.Monad.State
 import Data.List
 import Data.Maybe
 import System.IO
+import Control.Exception(bracket)
+
+import Debug.Trace as DT
 
 data PDBBackend = PDBBackend
     { pdbHandle :: Handle
@@ -45,17 +55,13 @@ openPDBOutput ::
     OutputSettings
     -> Dump
     -- ^Current state of the simulation
-    -> [String]
-    -- ^Names of the chains (in order of their numbers)
-    -> [String]
-    -- ^Names of the binder types (in order of their numbers)
     -> Bool
     -- ^Use simple PDB names?
     -> Bool
     -- ^Output intermediate steps?
     -> IO PDBBackend
     -- ^Opened PDB backend
-openPDBOutput OutputSettings{..} dump chainNames binderTypesNames simplePDB intermediate = do
+openPDBOutput OutputSettings{..} dump simplePDB intermediate = do
     let pdbFile = outputPrefix ++ ".pdb"
         laminFile = outputPrefix ++ "-lamin.pdb"
         metaFile = pdbFile ++ ".meta"
@@ -65,13 +71,15 @@ openPDBOutput OutputSettings{..} dump chainNames binderTypesNames simplePDB inte
     when (length chainNames /= length chIds)
         $ error "Fatal error: the number of chain names differs from the number of chains"
     when (length binderTypesNames /= length bts)
-        $ error "Fatal error: the number of binder type names differs from the number of binder types"
+        $ error ("Fatal error: the number of binder type names differs from the number of binder types" ++ show binderTypesNames ++ show bts)
     let chs = zip chIds chainNames
         mkMeta = if simplePDB then mkSimplePDBMeta else mkPDBMeta
         meta = fromMaybe (error pdbError) $ mkMeta evs (zip bts binderTypesNames) chs
     pdbHandle <- openFile pdbFile WriteMode
     withFile laminFile WriteMode $ \h -> pushPDBLamins h meta dump
-    return PDBBackend{..}
+    let ret = PDBBackend{..}
+    pushPDBStep ret dump 0 0 (-1) -- TODO 0
+    return ret
   where
     pdbError = "The PDB format can't handle this number of different beads, binders or chains."
 
@@ -96,3 +104,47 @@ pushPDBLamins handle pdbMeta dump' = do
   where
     filterLamins d = Dump { dumpBinders = filter isLamin $ dumpBinders d, dumpChains = [] }
     isLamin b = b ^. binderType == laminType
+
+
+data PDBReader = PDBReader
+    { handles :: [Handle]
+    , revMeta :: RevPDBMeta
+    , maxChainDistSquared :: Int
+    }
+
+openPDBInput :: InputSettings -> Int -> IO PDBReader
+openPDBInput InputSettings{..} maxChainDistSquared = do
+    handles <- mapM (`openFile` ReadMode) inputFiles
+    let mf = fromMaybe (error "Specify an input meta file") metaFile
+    revMeta <- eitherFail "Meta file read error: " $ withFile mf ReadMode readPDBMeta
+    return PDBReader{..}
+
+withPDBInput :: InputSettings -> Int -> (PDBReader -> [String] -> [String]-> IO a) -> IO a
+withPDBInput s dist f = bracket (openPDBInput s dist) close (\r -> f r (chainN r) (binderTN r))
+  where close PDBReader{..} = mapM_ hClose handles
+        chainN = getChainNames . revMeta
+        binderTN = getBinderTypesNames . revMeta
+
+instance (MonadIO m, ReadRepresentation m repr) => MoveProducer m repr PDBReader where
+    getMove PDBReader{..} repr score = do
+        liftIO $ putStrLn "asdf"
+        eof <- liftIO $ or <$> mapM hIsEOF handles
+        DT.traceM ("ISE" ++ show eof)
+        if eof then return Stop
+               else do
+                   dump <- makeDump repr
+                   dump' <- liftIO $ eitherFail "PDB read error: " $
+                                readPDB handles revMeta $ Just maxChainDistSquared
+                   let move = either (error "diff error") id $ diffDumps dump dump'
+                   score' <- updateCallback repr score move
+                   return $ MakeMove move score'
+
+skipPDBInput :: PDBReader -> Int -> IO Dump
+skipPDBInput PDBReader{..} 0 = 
+    eitherFail "PDB read error: " $ readPDB handles revMeta $ Just maxChainDistSquared
+skipPDBInput reader@PDBReader{..} n =  do
+    _ <- eitherFail "PDB read error: " $ readPDB handles revMeta $ Just maxChainDistSquared
+    skipPDBInput reader (n - 1)
+
+eitherFail :: String -> IO (Either String a) -> IO a
+eitherFail errorPrefix m = m >>= either (fail . (errorPrefix ++)) pure
