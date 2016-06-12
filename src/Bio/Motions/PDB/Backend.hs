8a7dd5bc7f900b9579eab1364a8373804f432914
Bartosz Stebel <bartoszstebel@gmail.com>
2016-06-12 22:19:03 +0200

    Input backends and MoveProducer
    This also fixes a bug in pdb output - first frame was never written
    
    Change-Id: I73380161714caf519ffda6f30b1e440fafdb5554

--- src/Bio/Motions/PDB/Backend.hs
+++ src/Bio/Motions/PDB/Backend.hs
@@ -6,20 +6,34 @@ Stability   : experimental
 Portability : unportable
 -}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE LambdaCase #-}
 module Bio.Motions.PDB.Backend where
 
 import Bio.Motions.Common
 import Bio.Motions.Output
+import Bio.Motions.Input
 import Bio.Motions.Types
 import Bio.Motions.PDB.Write
+import Bio.Motions.PDB.Read
+import Bio.Motions.PDB.Internal(RevPDBMeta,mergeDumps,ReadError)
 import Bio.Motions.PDB.Meta
 import Bio.Motions.Representation.Dump
+import Bio.Motions.Representation.Class
+import Bio.Motions.Callback.Class
 
 import Control.Lens
 import Control.Monad.State
+import Control.Monad.Except
 import Data.List
+import qualified Data.ByteString.Char8 as BS
 import Data.Maybe
+import Data.IORef
 import System.IO
+import System.IO.Error
+import Control.Exception(bracket)
 
 data PDBBackend = PDBBackend
     { pdbHandle :: Handle
@@ -45,17 +59,13 @@ openPDBOutput ::
     OutputSettings
     -> Dump
     -- ^Current state of the simulation
-    -> [String]
-    -- ^Names of the chains (in order of their numbers)
-    -> [String]
-    -- ^Names of the binder types (in order of their numbers)
     -> Bool
     -- ^Use simple PDB names?
     -> Bool
     -- ^Output intermediate steps?
     -> IO PDBBackend
     -- ^Opened PDB backend
-openPDBOutput OutputSettings{..} dump chainNames binderTypesNames simplePDB intermediate = do
+openPDBOutput OutputSettings{..} dump simplePDB intermediate = do
     let pdbFile = outputPrefix ++ ".pdb"
         laminFile = outputPrefix ++ "-lamin.pdb"
         metaFile = pdbFile ++ ".meta"
@@ -96,3 +106,90 @@ pushPDBLamins handle pdbMeta dump' = do
   where
     filterLamins d = Dump { dumpBinders = filter isLamin $ dumpBinders d, dumpChains = [] }
     isLamin b = b ^. binderType == laminType
+
+data PDBInFile = PDBInFile 
+    { handle :: Handle
+    , end :: Bool
+    , lastDump :: Dump
+    }
+
+data PDBReader = PDBReader
+    { files :: [IORef PDBInFile]
+    , revMeta :: RevPDBMeta
+    , maxChainDistSquared :: Int
+    }
+
+openPDBInput :: InputSettings -> Int -> IO PDBReader
+openPDBInput InputSettings{..} maxChainDistSquared = do
+    let mf = fromMaybe (error "Specify an input meta file") metaFile
+    revMeta <- eitherFail "Meta file read error: " $ withFile mf ReadMode readPDBMeta
+    files <- mapM (openSingle revMeta) inputFiles
+    return PDBReader{..}
+  where
+    openSingle :: RevPDBMeta -> FilePath -> IO (IORef PDBInFile)
+    openSingle revMeta path = do
+        handle <- openFile path ReadMode
+        frame <- readPDBData handle
+        let end = False
+            lastDump = case parseFrame frame >>= fromPDBData revMeta (Just maxChainDistSquared) of
+                         Left e -> error e
+                         Right d -> d
+        newIORef PDBInFile{..}
+
+withPDBInput :: InputSettings -> Int -> (PDBReader -> [String] -> [String]-> IO a) -> IO a
+withPDBInput s dist f = bracket (openPDBInput s dist) close (\r -> f r (chainNames r) (binderTypesNames r))
+  where
+    close :: PDBReader -> IO ()
+    close PDBReader{..} = mapM_ (readIORef >=> hClose . handle) files
+    chainNames = getChainNames . revMeta
+    binderTypesNames = getBinderTypesNames . revMeta
+
+instance (MonadIO m, ReadRepresentation m repr) => MoveProducer m repr PDBReader where
+    getMove reader repr score = do
+        dump <- makeDump repr
+        getMove' reader dump >>= \case
+            Nothing -> return Stop
+            Just move -> do
+                score' <- updateCallback repr score move
+                return $ MakeMove move score'
+
+getMove' :: MonadIO m => PDBReader -> Dump -> m (Maybe Move)
+getMove' reader dump = eitherFail "PDB read error: " . runExceptT $
+    getFrame reader >>= mapM (either diffError return . diffDumps dump)
+  where
+    diffError e = throwError $ "adjacent frames don't match: " ++ e
+
+getFrame :: (MonadIO m, MonadError ReadError m) => PDBReader -> m (Maybe Dump)
+getFrame PDBReader{..} = do
+    isEnd <- liftIO $ all end <$> mapM readIORef files
+    if isEnd then return Nothing
+         else fmap Just $ do
+             newDumps <- liftIO $ mapM (fmap lastDump . readIORef) files
+             forM_ files $ \x -> liftIO (readIORef x) >>= advanceSingleFile >>= liftIO . writeIORef x
+             liftEither $ mergeDumps newDumps
+  where
+      advanceSingleFile :: (MonadIO m, MonadError ReadError m) => PDBInFile -> m PDBInFile
+      advanceSingleFile file@PDBInFile{..} = do
+          (dump', end') <- readCatch handle >>= \case
+                            Nothing -> return (lastDump, True)
+                            Just frame -> parse frame >>= \d -> return (d, False)
+          return file{lastDump = dump', end = end'}
+      readCatch :: MonadIO m => Handle -> m (Maybe [BS.ByteString])
+      readCatch hdl = liftIO $ catchIOError (Just <$> readPDBData hdl)
+                    (\e -> if isEOFError e then return Nothing else ioError e)
+
+      parse :: (MonadError ReadError m) => [BS.ByteString] -> m Dump
+      parse x = liftEither $ parseFrame x >>= fromPDBData revMeta (Just maxChainDistSquared)
+
+skipPDBInput :: PDBReader -> Int -> IO Dump
+skipPDBInput reader 0 =
+    fromMaybe (error "unexpected end of file") <$> (eitherFail "PDB read error: " . runExceptT $ getFrame reader)
+skipPDBInput reader@PDBReader{..} n = do
+    _ <- eitherFail "PDB read error: " . runExceptT $ getFrame reader
+    skipPDBInput reader (n - 1)
+
+eitherFail :: (MonadIO m) => String -> m (Either String a) -> m a
+eitherFail errorPrefix m = m >>= either (fail . (errorPrefix ++)) pure
+
+liftEither :: (MonadError e m) => Either e a -> m a
+liftEither = either throwError return
