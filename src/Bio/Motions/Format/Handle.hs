26d117464b42114785098f65dc4ba6f0f3355149
Bartosz Stebel <bartoszstebel@gmail.com>
2016-06-12 22:19:03 +0200

    Input backends and MoveProducer
    Change-Id: I73380161714caf519ffda6f30b1e440fafdb5554

--- src/Bio/Motions/Format/Handle.hs
+++ src/Bio/Motions/Format/Handle.hs
@@ -6,35 +6,59 @@ Stability   : experimental
 Portability : unportable
 -}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TupleSections #-}
 module Bio.Motions.Format.Handle
     ( BinaryBackend
     , openBinaryOutput
+    , openBinaryInput
+    , withBinaryInput
+    , seekBinaryKF
     ) where
 
 import Bio.Motions.Format.ProtoStream
 import Bio.Motions.Format.DumpSerialisation
+import Bio.Motions.Format.DumpDeserialisation
+import qualified Bio.Motions.Format.Proto.Header as ProtoHeader
 
 import Bio.Motions.Representation.Dump
-import Bio.Motions.Callback.Class
+import Bio.Motions.Representation.Class
+import qualified Bio.Motions.Callback.Class as CC
 import Bio.Motions.Types
 import Bio.Motions.Output
+import Bio.Motions.Input
 
 import Foreign.C.String
 import Foreign.C.Types
 import Foreign.Ptr
+import Foreign.Storable
+import Foreign.ForeignPtr
 import Data.ByteString.Unsafe
 import qualified Data.ByteString.Lazy as BL
 import Text.ProtocolBuffers
 import Data.IORef
+import Data.Maybe
 
-type OutputHandle = Ptr HStream
+import Control.Monad
+import Control.Exception(bracket)
+import Control.Monad.IO.Class
+type BHandle = Ptr HStream
+data Mode = Reading | Appending
 
 data BinaryBackend = BinaryBackend
-    { handle :: OutputHandle
+    { handle :: BHandle
     , framesPerKF :: Int
     -- ^Frames per keyframe
     , framesSinceLastKF :: IORef Int
-    -- ^Frames written since last keyframe (including that keyframe)
+    -- ^Frames written/read since last keyframe (including that keyframe)
+    , mode :: Mode
+    , keyframeIterator :: IORef (Ptr HKeyframeIterator)
+    , deltaIterator :: IORef (Ptr HDeltaIterator)
+    , header :: ProtoHeader.Header
     }
 
 instance OutputBackend BinaryBackend where
@@ -46,6 +70,9 @@ instance OutputBackend BinaryBackend where
                       PushMove $ appendDelta state
 
     closeBackend BinaryBackend{..} = protoClose handle
+                >> freeRef keyframeIterator protoFreeKeyframeIterator
+                >> freeRef deltaIterator protoFreeDeltaIterator
+        where freeRef r f = readIORef r >>= f
     pushLastFrame _ _ _ _ = pure ()
 
 -- |Create a 'BinaryBackend'
@@ -53,13 +80,14 @@ openBinaryOutput ::
        Int
     -- ^Number of frames per keyframe
     -> OutputSettings
-    ->  Dump
-    -> [String]
-    -- ^Full names of the chains
+    -> Dump
     -> IO BinaryBackend
-openBinaryOutput framesPerKF OutputSettings{..} dump chainNames = do
+openBinaryOutput framesPerKF OutputSettings{..} dump = do
     handle <- openOutput
-    framesSinceLastKF <- newIORef 0
+    framesSinceLastKF <- newIORef 1
+    let mode = Appending
+    keyframeIterator <- newIORef nullPtr
+    deltaIterator <- newIORef nullPtr
     let st = BinaryBackend{..}
     appendKeyframe st dump ([], []) 0
     return st
@@ -73,7 +101,7 @@ openBinaryOutput framesPerKF OutputSettings{..} dump chainNames = do
 
 -- |Append a protobuf value to a stream, using a protostream function
 genericAppend :: (ReflectDescriptor msg, Wire msg) =>
-                    OutputHandle
+                    BHandle
                     -- ^Handle to a protostream object
                  -> (Ptr HStream -> Ptr () -> CSize -> IO ())
                     -- ^libprotostream handler function
@@ -86,12 +114,101 @@ genericAppend stream f msg =
     --TODO toStrict is slow
   where bytes = BL.toStrict . messagePut $ msg
 
-appendKeyframe :: BinaryBackend -> Dump -> Callbacks -> StepCounter -> IO ()
+appendKeyframe :: BinaryBackend -> Dump -> CC.Callbacks -> StepCounter -> IO ()
 appendKeyframe BinaryBackend{..} dump callbacks counter = do
     writeIORef framesSinceLastKF 1
     genericAppend handle protoAppendKeyframe $ getKeyframe dump callbacks counter
 
-appendDelta :: BinaryBackend -> Move -> Callbacks -> StepCounter -> IO ()
+appendDelta :: BinaryBackend -> Move -> CC.Callbacks -> StepCounter -> IO ()
 appendDelta BinaryBackend{..} move callbacks counter = do
     modifyIORef framesSinceLastKF (+1)
     genericAppend handle protoAppendDelta $ serialiseMove move callbacks counter
+
+openBinaryInput :: InputSettings -> IO BinaryBackend
+openBinaryInput InputSettings{..} = do
+    handle <- case inputFiles of
+                [file] -> withCString file protoOpenExisting
+                _ -> fail "Specify exactly one binary input file"
+    framesPerKF <- fromIntegral <$> protoGetFPKF handle
+    framesSinceLastKF <- newIORef 0
+    let mode = Reading
+    kfIt <- protoIterKeyframes handle
+    deltaIterator <- protoIterDeltas kfIt >>= newIORef
+    keyframeIterator <- newIORef kfIt
+    header <- genericGet (protoGetHeader handle) protoFreeHeader id
+    return BinaryBackend{..}
+
+withBinaryInput :: InputSettings -> (BinaryBackend -> Dump -> [String] -> IO a) -> IO a
+withBinaryInput s f = bracket open (closeBackend . fst) (\(backend, dump) -> f backend dump (getNames backend))
+    where open = openBinaryInput s >>= \backend -> (backend,) <$> seekBinaryKF backend 0
+          getNames = fromMaybe (error "error getting chain names") . getChainNames . header
+
+-- | Seek `i` frames from the beginning and return the Dump at this position. `i` must be divisible by fpkf.
+seekBinaryKF :: BinaryBackend -> Int -> IO Dump
+seekBinaryKF BinaryBackend{..} i = do
+    when (0 /= i `mod` framesPerKF) $ fail "not divisible by fpkf"
+    kfi <- readIORef keyframeIterator
+    protoAdvanceKeyframeIterator kfi $ fromIntegral (i `div` framesPerKF)
+    valid <- (/= 0) <$> protoValidKeyframeIterator handle kfi
+    unless valid $ error "out of bounds"
+    readIORef deltaIterator >>= protoFreeDeltaIterator
+    protoIterDeltas kfi >>= writeIORef deltaIterator
+    writeIORef framesSinceLastKF 1
+    liftIO $ genericGet (protoGetKeyframe kfi) protoFreeKeyframe $
+        fromMaybe (error "invalid dump") . deserialiseDump header
+
+-- |Get a value from a stream using a protostream getter and its associated deallocation function
+genericGet :: (Wire msg, ReflectDescriptor msg) =>
+       (Ptr (Ptr ()) -> Ptr CSize -> IO ())
+       -- ^protostream getter function
+    -> (Ptr () -> IO ())
+       -- ^ protostream finalizer (i.e. free)
+    -> (msg -> b)
+       -- ^ deserialising function
+    -> IO b
+genericGet getter finalizer fun = do
+    ptrPtr' <- mallocForeignPtr
+    sizePtr' <- mallocForeignPtr
+    withForeignPtr ptrPtr' $ \ptrPtr ->
+        withForeignPtr sizePtr' $ \sizePtr -> do
+            getter ptrPtr sizePtr
+            buf <- peek ptrPtr
+            size <- fromIntegral <$> peek sizePtr
+            bs <- unsafePackCStringFinalizer (castPtr buf) size $ finalizer buf
+            let msg = case messageGet $ BL.fromStrict bs of
+                    Right (m, _) -> m
+                    Left e -> error $ "protobuf decoding error: " ++ e
+            return $ fun msg
+
+instance (MonadIO m, ReadRepresentation m repr) => MoveProducer m repr BinaryBackend where
+    getMove BinaryBackend{..} repr score = do
+        delta <- liftIO (readIORef deltaIterator)
+        kfi <- liftIO (readIORef keyframeIterator)
+        valid <- liftIO (protoValidDeltaIterator kfi delta)
+        ret <- case valid of
+                 0 -> do
+                    liftIO $ protoAdvanceKeyframeIterator kfi 1
+                    liftIO (protoValidKeyframeIterator handle kfi) >>= \case
+                        0 -> return Nothing     --TODO end of file?
+                        _ -> do
+                            ret <- getDiffed kfi
+                            liftIO $ protoFreeDeltaIterator delta
+                            liftIO $ protoIterDeltas kfi >>= writeIORef deltaIterator
+                            return $ Just ret
+                 _ -> do
+                    ret <- liftIO $ getDelta delta
+                    liftIO $ protoAdvanceDeltaIterator delta
+                    return $ Just ret
+        forM ret $ \move -> (move,) <$> CC.updateCallback repr score move
+      where
+        getDelta delta = genericGet (protoGetDelta delta) protoFreeDelta $ \msg ->
+            fromMaybe (error "failed to decode Move") $ deserialiseMove msg
+
+        getDiffed kfi = do
+            dump' <- liftIO $ genericGet (protoGetKeyframe kfi) protoFreeKeyframe $
+                fromMaybe (error "failed to load Dump") . deserialiseDump header
+            dump <- makeDump repr
+            case diffDumps dump dump' of
+              Right m -> return m
+              Left e -> error $ "dump difference error: " ++ e
+    {-# INLINABLE getMove #-}
